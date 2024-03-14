a98365f30587baa5f450c10a7c3ca895f4bfd296
Eric Chen <hkisthebest@gmail.com>
2024-03-13 20:50:06 -0700

    debug

--- server/db/connection.go
+++ server/db/connection.go
@@ -10,10 +10,11 @@ import (
 var Pool *pgxpool.Pool
 
 func Connect() {
-  dbpool, err := pgxpool.New(context.Background(), os.Getenv("DB_URL"))
+  var err error
+  Pool, err = pgxpool.New(context.Background(), os.Getenv("DB_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
 		os.Exit(1)
 	}
-  Pool = dbpool
+  //Pool = dbpool
 }
