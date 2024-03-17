d8278da221d7bae3ce1c76b9678ccff26f71dfbf
Eric Chen <hkisthebest@gmail.com>
2024-03-16 17:57:13 -0700

    fix query and style change

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,49 +1,73 @@
 import './App.css'
 import Chart from './chart'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { DateTime } from 'luxon'
-import useSearch from './hook'
-import { Datapoint } from './types/dataset'
+import search from './hook'
+import { Datapoint, DatapointResponse } from './types/dataset'
 import GitHubButton from 'react-github-btn'
 
+const durations: number[] = [24, 72, 240, 720, 1200]
+
 function App() {
-  const [subredditInput, setsubredditInput] = useState<string>('')
-  const [duration, setDuration] = useState<string>('24')
-  const { result, loading } = useSearch(subredditInput, duration)
+  const [subredditInput, setSubredditInput] = useState<string>('')
+  const [duration, setDuration] = useState<number>(24)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [result, setResult] = useState<DatapointResponse>({})
+
+  useEffect(() => {
+    search('', 24, setLoading, setResult)
+  }, [])
 
   if (loading) return null
 
   return (
-    <>
-      <div style={{ marginTop: '10px', marginLeft: '10px', position: 'fixed', top: '0px', left: '0px' }}>
+    <div style={{ flexDirection: 'row' }}>
+      <div style={{
+        width: '20%',
+        float: 'left',
+        paddingLeft: 15
+      }}>
         <h2 >Active subreddit users</h2>
-        r/ <input value={subredditInput} onChange={(e) => setsubredditInput(e.target.value)} />
-        <select onChange={(e) => setDuration(e.target.value)}>
-          <option value="24">1 day</option>
-          <option value="72">3 days</option>
-          <option value="240">10 days</option>
-          <option value="720">30 days</option>
-          <option value="1200">50 days</option>
+        r/ <input value={subredditInput}
+          onChange={(e) => setSubredditInput(e.target.value)}
+          placeholder='Three characters...'
+        />
+        <select onChange={(e) => setDuration(+e.target.value)} value={duration}>
+          {durations.map(d => (
+            <option key={d} value={d}>{d / 24} day{d === 24 ? '' : 's'}</option>
+          ))}
         </select>
-        <p style={{ fontWeight: 'bold' }}>All the time is shown in UTC</p>
+        <input type='button'
+          onClick={() => {
+            search(subredditInput, duration, setLoading, setResult)
+          }}
+          value="Search" />
+        <p
+          style={{ fontWeight: 'bold' }}>
+          Time is shown in UTC
+        </p>
         <div style={{ display: 'flex', marginTop: 20, justifyContent: 'start' }}>
-          <GitHubButton href="https://github.com/hkisthebest/redditdb" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" data-show-count="true" aria-label="Star hkisthebest/redditdb on GitHub">Star</GitHubButton>
+          <GitHubButton href="https://github.com/hkisthebest/redditdb"
+            data-color-scheme="no-preference: light; light: light; dark: dark;"
+            data-size="large"
+            data-show-count="true"
+            aria-label="Star hkisthebest/redditdb on GitHub"
+          >
+            Star
+          </GitHubButton>
         </div>
       </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', marginTop: 50 }}>
-        {Object.entries(result).map(([subreddit, data]: [string, Datapoint[]]) => {
-          return (
-            <div key={data?.[0].id} style={{ boxSizing: 'border-box', }}>
-              <Chart title={subreddit} datasets={data.map((d: Datapoint) => {
-                const x = DateTime.fromISO(d.time).toFormat('MMM, d, h a')
-                return { x, y: d.users }
-              })
-              } />
-            </div>
-          )
-        })}
-      </ div>
-    </>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {Object.entries(result).map(([subreddit, data]: [string, Datapoint[]]) => (
+          <div key={subreddit} style={{ width: '375px', boxSizing: 'border-box', padding: '5px' }}>
+            <Chart title={subreddit} datasets={data.map((d: Datapoint) => ({
+              x: DateTime.fromISO(d.time).toFormat('MMM, d, h a'),
+              y: d.users,
+            }))} />
+          </div>
+        ))}
+      </div>
+    </ div>
   )
 }
 
--- app/src/hook.tsx
+++ app/src/hook.tsx
@@ -1,35 +1,22 @@
-import { useEffect, useState } from 'react'
+import { Dispatch, SetStateAction } from 'react'
 import { DatapointResponse } from './types/dataset'
 const apiHost: string = import.meta.env.VITE_API_HOST || 'http://localhost:3000'
-export default function useSearch(input: string, duration: string) {
-  const [result, setResult] = useState<DatapointResponse>({})
-  const [loading, setLoading] = useState<boolean>(false)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true)
-      try {
-        let response
-        if (input.length >= 3 && input) {
-          response = await fetch(`${apiHost}/api/datapoints/${input}?duration=${duration}`)
-        } else {
-          response = await fetch(`${apiHost}/api/datapoints/top?duration=${duration}`)
-        }
-        const resultJson = await response.json() as DatapointResponse
-        setResult(resultJson)
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false)
-      }
+export default async function search(input: string, duration: number, setLoading: Dispatch<SetStateAction<boolean>>, setResult: Dispatch<SetStateAction<DatapointResponse>>) {
+  try {
+    setLoading(true)
+    let url = `${apiHost}/api/datapoints/top?duration=${duration}`
+    if (input.length >= 3) {
+      url = `${apiHost}/api/datapoints/${input}?duration=${duration}`
     }
+    const response = await fetch(url)
+    const resultJson = await response.json() as DatapointResponse
+    setResult(resultJson)
+  } catch (error) {
 
-    fetchData()
-  }, [input, duration])
-
-  return {
-    result,
-    loading
+    console.error('Error fetching data:', error);
+  } finally {
+    setLoading(false)
   }
-}
 
+}
