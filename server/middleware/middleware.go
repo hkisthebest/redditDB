c1d2aea29904b6d3ce1b630d22273d9d7269b0f5
Eric Chen <hkisthebest@gmail.com>
2024-03-19 18:32:08 -0700

    prom name space

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -28,7 +28,7 @@ func MeasureResponseDuration(next http.Handler) http.Handler {
 	buckets := []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10}
 
 	responseTimeHistogram := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: "grafanacloud_hkisthebest_prom",
+		Namespace: "redditdb_response_time",
 		Name:      "http_server_request_duration_seconds",
 		Help:      "Histogram of response time for handler in seconds",
 		Buckets:   buckets,
